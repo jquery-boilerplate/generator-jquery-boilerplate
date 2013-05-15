3506ffa7c186c52377b49a0c39c95b070881262b
Zeno Rocha <zno.rocha@gmail.com>
2013-05-15 15:39:27 -0300

    Fix test command

--- test/test.js
+++ test/test.js
@@ -4,15 +4,18 @@
 var path = require('path');
 var helpers = require('yeoman-generator').test;
 
-describe('jQuery generator test', function () {
+describe('jQuery Boilerplate generator test', function () {
   beforeEach(function (done) {
     helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
       if (err) {
         return done(err);
       }
 
-      this.app = helpers.createGenerator('jquery:app', [
-        '../../app'
+      this.app = helpers.createGenerator('jquery-boilerplate:app', [
+        '../../app', [
+          helpers.createDummyGenerator(),
+          'mocha:app'
+        ]
       ]);
       done();
     }.bind(this));
